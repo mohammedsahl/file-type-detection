adf793237ffd4f98769898f9b37c56ea4d0c0e02
Mohammed Sahl <sahl.kably@gmail.com>
2020-07-06 19:10:36 -0400

    Clean up and add scripts
    Add script for trie

--- trie.ts
+++ trie.ts
@@ -7,19 +7,19 @@ export class Trie {
     constructor() {
         this.tree = new trieNode();
     }
-    private _walk(keys: string[], node: trieNode, level: number): void {
+    private _traversePrint(keys: string[], node: trieNode, level: number): void {
         keys = keys.filter(key => key !== "value");
         for (let [index, key] of keys.entries()) {
             //Prints values with indentation
             console.log(`${"| ".repeat(level - 1)}|-> ${level}.${index + 1} ${node[key].value}`);
             
             //Recursion step
-            this._walk(Object.keys(node[key]), node[key], level + 1);
+            this._traversePrint(Object.keys(node[key]), node[key], level + 1);
         }
         return;
     }
 
-    private _walkInsert(node: trieNode, string: string, index: number): trieNode {
+    private _traverseInsert(node: trieNode, string: string, index: number): trieNode {
         if (index === string.length - 1) {
             return (node[string[index]] = { value: string })
         } else {
@@ -29,14 +29,14 @@ export class Trie {
 
             node[string[index]][string[index + 1]] = {
                 ...node[string[index]][string[index + 1]],
-                ...this._walkInsert(node[string[index]], string, index + 1)
+                ...this._traverseInsert(node[string[index]], string, index + 1)
             }
         }
 
         return node[string[index]];
     }
 
-    private _walkSearch(string: string, node: trieNode, index: number): string {
+    private _traverseSearch(string: string, node: trieNode, index: number): string {
         if (!node) {
             console.error("Not there lul!");
             return "INVALID";
@@ -46,21 +46,21 @@ export class Trie {
             console.log("Got em'");
             return node.value;
         } else {
-            return this._walkSearch(string, node[string[index]], index + 1);
+            return this._traverseSearch(string, node[string[index]], index + 1);
         }
     }
 
     public insert(string: string): void {
-        this._walkInsert(this.tree, string, 0)
+        this._traverseInsert(this.tree, string, 0)
     }
 
     public search(string: string): void {
-        console.log(this._walkSearch(string, this.tree, 0));
+        console.log(this._traverseSearch(string, this.tree, 0));
     }
 
     public print(): void {
         console.log("Tree");
-        this._walk(Object.keys(this.tree), this.tree, 1);
+        this._traversePrint(Object.keys(this.tree), this.tree, 1);
     }
 }
 
